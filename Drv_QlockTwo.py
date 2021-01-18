a2fab93a7df53b08d5b5b1542285073959d80da6
Wunder0 <andreas.kuehefuss@web.de>
2021-01-18 18:02:43 +0100

    Add Interface Prototype for Drv_QlockTwo driver lib

---
+++ Drv_QlockTwo.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+class Drv_QlockTwo:
+    
+    def __init__(self, num_letter_vertical = 10, num_letter_horizontal = 11, leds_per_letter = 2):
+        self.__num_letter_vertical = num_letter_vertical;
+        self.__num_letter_horizontal = num_letter_horizontal;
+        self.__leds_per_letter = leds_per_letter;
+        
+        self.__font_color = color;
+        self.__font_brightness
+        self.__frame_color
+        self.__frame_brightness
+        self.__minute_color
+        self.__minute_brightness
+        self.__general_brightness
+        self.__is_minutes_shown
+        self.__is_frame_shown
+        self.__is_soft_transistion_enabled
+        self.__transition_time
+        self.__transition_mode
+        
+    def enable_letter(pos_vertical, pos_horizontal):
+        k
+        
+    def disable_letter(pos_vertical, pos_horizontal):
+        k
+        
+    def update_letter_matrix(letter_matrix):
+        
+    def flush():
+        
+    def flush_sync():
+        
+    def set_font_color(font_color):
+        self.__font_color = font_color
+        
+    def set_font_brightness(font_brightness):
+        self.__font_brightness = font_brightness
+        
+    def set_frame_color(frame_color):
+        self.__frame_color = frame_color
+        
+    def set_frame_brightness(frame_brightness):
+        self.__frame_brightness = frame_brightness
+        
+    def set_minute_color(minute_color):
+        self.__minute_color = minute_color
+        
+    def set_minute_brightness(minute_brightness):
+        self.__minute_brightness = minute_brightness
+        
+    def set_general_brightness(general_brightness):
+        self.__general_brightness = general_brightness
+        
+    def set_is_minutes_shown(is_minutes_shown):
+        self.__is_minutes_shown = is_minutes_shown
+        
+    def set_is_frame_shown(is_frame_shown):
+        self.__is_frame_shown = is_frame_shown
+        
+    def set_is_soft_transistion_enabled(is_soft_transistion_enabled):
+        self.__is_soft_transistion_enabled = is_soft_transistion_enabled
+        
+    def set_transition_time(transition_time):
+        self.__transition_time = transition_time
+        
+    def set_transition_mode(transition_mode):
+        self.__transition_mode = transition_mode
+        
