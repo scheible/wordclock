67ce1c71a139f86b6c8cab016e2037833582fecc
scheible <patrikscheible@posteo.net>
2021-03-14 17:32:28 +0100

    cleanup no 2

--- ap_config/start_ap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-wpa_cli -i wlan0 disconnect
-python3 change_dhcpcd_config.py static
-dhcpcd -k wlan0
-systemctl stop wpa_supplicant
-dhcpcd -n wlan0
-systemctl start dnsmasq
-systemctl start hostapd
--- ap_config/stop_ap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-python3 change_dhcpcd_config.py dhcp
-dhcpcd -k wlan0
-systemctl stop dnsmasq
-systemctl stop hostapd 
-dhcpcd -n wlan0
-systemctl start wpa_supplicant
-wpa_cli -i wlan0 reconfigure
-wpa_cli -i wlan0 select_network 0
