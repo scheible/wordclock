e96febffd861844e241e1176b790d903b46b6780
Wunder0 <andreas.kuehefuss@web.de>
2021-03-19 23:13:02 +0100

    add binding

--- bin/shared/ledBinding.py
+++ bin/shared/ledBinding.py
@@ -5,7 +5,7 @@ from rpi_ws281x import *
 
 class LedBinding:
     
-    def __init__(self, ws2812bJson, numLeds):
+    def __init__(self, ws2812bJson, numLeds, brightness):
         
         # LED strip configuration:
         self.__LED_COUNT      = numLeds # Number of LED pixels.
@@ -13,7 +13,7 @@ class LedBinding:
         #LED_PIN        = 10             # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
         self.__LED_FREQ_HZ    = ws2812bJson["ledFreqHz"]   # LED signal frequency in hertz (usually 800khz)
         self.__LED_DMA        = ws2812bJson["ledDma"]   # DMA channel to use for generating signal (try 10)
-        self.__LED_BRIGHTNESS = ws2812bJson["brightness"]      # Set to 0 for darkest and 255 for brightest
+        self.__LED_BRIGHTNESS = brightness      # Set to 0 for darkest and 255 for brightest
         self.__LED_INVERT     = ws2812bJson["ledInvert"]   # True to invert the signal (when using NPN transistor level shift)
         self.__LED_CHANNEL    = ws2812bJson["ledChannel"]   # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
