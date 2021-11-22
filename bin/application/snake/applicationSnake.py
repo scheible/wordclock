384fcb94fd91a5700aad4d53e9104f98bbbd673c
scheible <patrikscheible@posteo.net>
2021-11-22 19:10:00 +0100

    added basic game logic for snake

--- bin/application/snake/applicationSnake.py
+++ bin/application/snake/applicationSnake.py
@@ -1,6 +1,6 @@
 from application.application import *
 from shared.ledBinding import *
-import time
+import time, random
 
 class ApplicationSnake(Application):
     
@@ -12,7 +12,7 @@ class ApplicationSnake(Application):
         self.__ledBinding = LedBinding(jsonWs2812b, 220, 150)
         self.__ledBinding.clear()
         self.__ledMatrix = LEDMatrix(self.__ledBinding)
-        self.__snake = Snake(self.__ledMatrix)
+        self.__snake = SnakeGame(self.__ledMatrix)
         
     def applicationTask(self, jsonConfig, isUpdateJson):   
         
@@ -62,32 +62,153 @@ class LEDMatrix():
             index += (self.__yMax - y)*2
 
         self.__ledBinding.setLedRGB(index, r, g, b)
+        self.__ledBinding.setLedRGB(index+1, r, g, b)
     
     def show(self):
         self.__ledBinding.show()
 
 
-class Snake():
+class SnakeGame():
 
     def __init__(self, ledMatrix):
         self.__ledMatrix = ledMatrix
-        self.__posX = 2
-        self.__posY = 5
-        self.__xDir = 0
-        self.__yDir = 0
+        self.__snake = Snake(5,5)
+        self.__tickCounter = 0
+        self.__food = Food()
+        self.__food.place(self.__snake)
 
     def tick(self):
-        self.__posX += self.__xDir
-        self.__posY += self.__yDir
+        self.__tickCounter += 1
+        self.__snake.move()
 
-        if (self.__posX > 10):
-            self.__posX = 0
-        if (self.__posX < 0):
-            self.__posX = 10
-        if (self.__posY > 9):
-            self.__posY = 0
-        if (self.__posY < 0):
-            self.__posY = 9
+        if (self.__food.collisionWithSnake(self.__snake)):
+            self.__snake.eat()
+            self.__food.place(self.__snake)     
+
+    def changeDirection(self, direction):
+       self.__snake.changeDirection(direction)
+
+    def render(self):
+        self.__ledMatrix.clear()
+        self.__snake.render(self.__ledMatrix)
+        self.__food.render(self.__ledMatrix)
+        self.__ledMatrix.show()
+
+class Food():
+
+    def __init__(self):
+        pass
+
+    def place(self, snake):
+        while True:
+            self._x = random.randint(0,10)
+            self._y = random.randint(0,9)
+
+            if (not snake.checkCollision(self._x, self._y)):
+                break
+
+    def getX(self):
+        return self._x 
+
+    def getY(self):
+        return self._y
+
+    def render(self, ledMatrix):
+        if (self._x >= 0 and self._y >= 0):
+            ledMatrix.setRGB(self._x, self._y, 0, 150, 0)
+
+    def collisionWithSnake(self, snake):
+        return self.collision(snake.getX(), snake.getY())
+
+    def collision(self, x, y):
+        return (self._x == x and self._y == y)
+
+
+class SnakeLink():
+
+    def __init__(self):
+        self._x = -1
+        self._y = -1
+        self._prevLink = None
+
+    def render(self, ledMatrix):
+        if (self._x >= 0 and self._y >= 0):
+            ledMatrix.setRGB(self._x, self._y, 150, 0, 0)
+        
+        if (self._prevLink != None):
+            self._prevLink.render(ledMatrix)
+
+    def kill(self):
+        if (self._prevLink != None):
+            self._prevLink.kill()
+            del self._prevLink
+            self._prevLink = None
+
+    def eat(self):
+        #if __prevLink == None then we are the
+        #last link, in this case add to this
+        #if we are not the last link, pass it
+        #on until the last link will add the new
+        #link
+        if (self._prevLink == None):
+            self._prevLink = SnakeLink()
+        else:
+            self._prevLink.eat()
+
+    def getX(self):
+        return self._x
+
+    def getY(self):
+        return self._y
+
+    def move(self, x, y):
+        # first move the previous link to the position of this
+        # link. Then move this link to the new position
+        if (self._prevLink != None):
+            self._prevLink.move(self._x, self._y)
+
+        self._x = x 
+        self._y = y
+
+    def collisionDownTheLine(self, x, y):
+        # checks if this link or any link behind it
+        # collides with x and y
+        if (self._x == x and self._y == y):
+            return True
+        else:
+            if (self._prevLink != None):
+                return self._prevLink.collisionDownTheLine(x,y)
+            else:
+                return False
+
+
+class Snake(SnakeLink):
+
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+        self._prevLink = None
+        self.changeDirection(3)
+
+    def move(self):
+        if (self._prevLink != None):
+            self._prevLink.move(self._x, self._y)
+
+        self._x += self._xDir
+        self._y += self._yDir
+
+        if self._x > 10:
+            self._x = 0
+        if self._x < 0:
+            self._x = 10
+        if self._y > 9:
+            self._y = 0 
+        if self._y < 0:
+            self._y = 9
+
+        if self.checkSelfCollision():
+            print("kill snake!!")
+            self._prevLink.kill()
 
     def changeDirection(self, direction):
         # dir 0 -> up
@@ -95,20 +216,23 @@ class Snake():
         # dir 2 -> left
         # dir 3 -> right
         if (direction == 0):
-            self.__xDir = 0
-            self.__yDir = 1
+            self._xDir = 0
+            self._yDir = 1
         elif (direction == 1):
-            self.__xDir = 0
-            self.__yDir = -1
+            self._xDir = 0
+            self._yDir = -1
         elif (direction == 2):
-            self.__xDir = -1
-            self.__yDir = 0
+            self._xDir = -1
+            self._yDir = 0
         elif (direction == 3):
-            self.__xDir = 1
-            self.__yDir = 0
+            self._xDir = 1
+            self._yDir = 0
 
-    def render(self):
-        self.__ledMatrix.clear()
-        self.__ledMatrix.setRGB(self.__posX, self.__posY, 255, 0, 0)
-        self.__ledMatrix.show()
+    def checkSelfCollision(self):
+        return self.checkCollision(self._x, self._y)
 
+    def checkCollision(self, x, y):
+        if (self._prevLink != None):
+            return (self._prevLink.collisionDownTheLine(x, y))
+        else:
+            return False
